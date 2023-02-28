fef7416180ecb4e7a4a69a65260911d81bdccb81
Artem174a <Artem174a@yandex.ru>
2023-02-28 18:44:37 +0500

    add file

--- tgbot/functions/start.py
+++ tgbot/functions/start.py
@@ -1,7 +1,9 @@
 from aiogram.types import Message, Contact
 from tgbot.database.DB import Database as Db
 from tgbot.keyboards.inline import *
+from aiogram.types import ChatActions
 from tgbot.functions.account import *
+import openai
 
 
 async def admin_start(message: Message):
@@ -13,7 +15,14 @@ async def user_start(message: Message):
         Db().add_user(telegram_id=message.from_user.id,
                       name=message.from_user.first_name,
                       username=message.from_user.username)
-        await message.answer(f"<b>Привет, {message.from_user.first_name}!"
-                             f"Я новый бот который умеет всё!</b>\n\n", reply_markup=InlineKeyboard().get_account())
+        await message.answer(f'''
+<b>Привет, {message.from_user.first_name}!</b>
+Это новый бот который позволяет 
+пользоваться большим колличеством сервисов!
+        ''', reply_markup=InlineKeyboard().start())
     else:
-        await Account(message=message).get_account()
+        await message.answer(f'''
+<b>Привет, {message.from_user.first_name}!</b>
+Мы снова рады тебя видеть!
+        ''', reply_markup=InlineKeyboard().start())
+
--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -7,22 +7,39 @@ class InlineKeyboard:
         self.button = types.InlineKeyboardButton
         pass
 
-    def get_account(self):
+    def start(self):
         buttons = [
-            types.InlineKeyboardButton(text="Мой аккаунт 💼 ", callback_data="get_account")]
+            self.button(text="Мой аккаунт 💼 ", callback_data="back_to_account"),
+            self.button(text="Меню", callback_data="back_to_menu")]
         keyboard = self.inline_keyboard(row_width=1)
         keyboard.add(*buttons)
         return keyboard
 
-    def menu_account(self):
+    def menu(self):
         buttons = [
-            self.button(text="Подписка", callback_data="back_to_account"),
-            self.button(text="Инфо", callback_data="back_to_account"),
-            self.button(text="Поддержка", callback_data="back_to_account"),
-            self.button(text="Меню", callback_data="get_menu"),
+            self.button(text="Мой аккаунт 💼 ", callback_data="back_to_account"),
+            self.button(text="Сервисы", callback_data="back_to_services")]
+        keyboard = self.inline_keyboard(row_width=1)
+        keyboard.add(*buttons)
+        return keyboard
+
+    def account_menu(self):
+        buttons = [
+            self.button(text="Подписка", callback_data="subscribe_account"),
+            self.button(text="Инфо", callback_data="info_account"),
+            self.button(text="Поддержка", callback_data="support_account"),
+            self.button(text="Меню", callback_data="back_to_menu"),
         ]
         keyboard = self.inline_keyboard()
         keyboard.add(buttons[0])
         keyboard.add(buttons[1], buttons[2])
         keyboard.add(buttons[3])
+        return keyboard
+
+    def services(self):
+        buttons = [
+            self.button(text="ChatGPT", callback_data="ChatGPT_serv"),
+            self.button(text="Меню", callback_data="back_to_menu")]
+        keyboard = self.inline_keyboard(row_width=1)
+        keyboard.add(*buttons)
         return keyboard
