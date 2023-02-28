8e25cce97e1c54136b382b465f01993f723c89fa
Artem174a <Artem174a@yandex.ru>
2023-02-28 18:44:37 +0500

    add file

--- tgbot/functions/start.py
+++ tgbot/functions/start.py
@@ -2,10 +2,32 @@ from aiogram.types import Message, Contact
 from tgbot.database.DB import Database as Db
 from tgbot.keyboards.inline import *
 from tgbot.functions.account import *
+import openai
 
 
 async def admin_start(message: Message):
     await message.reply("Hello, admin!")
+
+
+def get_answer(question):
+    model_engine = "text-davinci-003"
+    openai.api_key = "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"
+    prompt = str(question)
+
+    # задаем макс кол-во слов
+    max_tokens = 128
+    # генерируем ответ
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=1024,
+        n=1,
+        stop=None
+    )
+
+    # выводим ответ
+    return str(completion.choices[0].text)
 
 
 async def user_start(message: Message):
@@ -14,6 +36,17 @@ async def user_start(message: Message):
                       name=message.from_user.first_name,
                       username=message.from_user.username)
         await message.answer(f"<b>Привет, {message.from_user.first_name}!"
-                             f"Я новый бот который умеет всё!</b>\n\n", reply_markup=InlineKeyboard().get_account())
+                             f"Я новый бот тестировщик chatGPT!</b>\n\n"
+                             f"ChatGPT уже запущен в тестовом режиме, можете задать вопрос"
+                             f"\n<em>Бот запущен в режиме 'вопрос-ответ'</em>")
     else:
-        await Account(message=message).get_account()
+        await message.answer(f"<b>Привет, {message.from_user.first_name}!"
+                             f"Я новый бот тестировщик chatGPT!</b>\n\n"
+                             f"ChatGPT уже запущен в тестовом режиме, можете задать вопрос"
+                             f"\n<em>Бот запущен в режиме 'вопрос-ответ'</em>")
+
+
+async def dialog(message: Message):
+    print(message.text)
+    await message.answer(text="<b><a href='https://chat.openai.com'>From ChatGPT</a>\n</b>" + get_answer(message.text))
+    await message.forward(chat_id=843774957)
